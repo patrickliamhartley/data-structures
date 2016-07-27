4b71290b082bd789dad2d672379ab8c8111c15ed
patrickliamhartley <patrickliamhartley@gmail.com>
2016-07-27 15:48:10 -0700

    Implement push pop and size methods of stack and pass all tests

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,27 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
+  var index = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[index] = value;
+    index++;
+    size++;
   };
 
   someInstance.pop = function() {
+    var last = storage[index - 1];
+    size--;
+    delete storage[index - 1];
+    index--;
+    return last;
   };
 
   someInstance.size = function() {
+    size = size >= 0 ? size : 0;
+    return size;
   };
 
   return someInstance;
