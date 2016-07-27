e4310200fa3b677542f9214d048997a41453ee41
patrickliamhartley <patrickliamhartley@gmail.com>
2016-07-27 15:31:50 -0700

    Implement size enqueue and dequeue methods and pass all specs

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,28 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
+  var index = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[index] = value;
+    index ++;
+    size ++;
   };
 
   someInstance.dequeue = function() {
+    var front = storage[index - size];
+    delete storage[index - size];
+    size --;
+    return front;
   };
 
   someInstance.size = function() {
+    size = size < 0 ? 0 : size;
+    return size;
+    
   };
 
   return someInstance;
