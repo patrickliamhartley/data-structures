7e10d66333e092f19aff94b3ffc78fdfef85140a
patrickliamhartley <patrickliamhartley@gmail.com>
2016-07-27 18:51:37 -0700

    Implement the stack and queue methods using the prototypal pattern

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,29 @@
 var Queue = function() {
+  var newQueue = Object.create(queueMethods);
+  newQueue.length = 0;
+  newQueue.index = 0;
+  return newQueue;
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function (val) {
+  this[this.index] = val;
+  this.length++;
+  this.index++;
+};
 
+queueMethods.dequeue = function () {
+  var temp = this[this.index - this.length];
+  delete this[this.index - this.length];
+  this.length--;
+  return temp;
+};
+
+queueMethods.size = function () {
+  this.length = this.length >= 0 ? this.length : 0;
+  return this.length;
+
+};
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,28 @@
 var Stack = function() {
+  var newStack = Object.create(stackMethods);
+  newStack.length = 0;
+  return newStack;
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 };
 
 var stackMethods = {};
+
+stackMethods.push = function (val) {
+  this[this.length] = val;
+  this.length++;
+};
+
+stackMethods.pop = function () {
+  this.length--;
+  var temp = this[this.length];
+  delete this[this.length];
+  return temp;
+};
+
+stackMethods.size = function () {
+  this.length = this.length >= 0 ? this.length : 0;
+  return this.length;
+};
 
 
