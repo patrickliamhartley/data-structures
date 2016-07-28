bcef9ccd4f7768dc74cb0296160bac318b8ddcf0
patrickliamhartley <patrickliamhartley@gmail.com>
2016-07-27 19:18:00 -0700

    Implement pseudoclassical stack methods and pass all tests

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,26 @@
 var Queue = function() {
+  this.length = 0;
+  this.index = 0;
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+};
+
+Queue.prototype.enqueue = function(val) {
+  this[this.index] = val;
+  this.length++;
+  this.index++;
+};
+
+Queue.prototype.dequeue = function() { 
+  var temp = this[this.index - this.length];
+  delete this[this.index - this.length];
+  this.length--;
+  return temp;
+};
+
+Queue.prototype.size = function() {
+  this.length = this.length >= 0 ? this.length : 0;
+  return this.length;
 };
 
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,22 @@
 var Stack = function() {
+  this.length = 0;
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 };
 
+Stack.prototype.push = function (val) {
+  this[this.length] = val;
+  this.length++;
+};
 
+Stack.prototype.pop = function () {
+  this.length--;
+  var temp = this[this.length];
+  delete this[this.length];
+  return temp;
+};
+
+Stack.prototype.size = function () {
+  this.length = this.length >= 0 ? this.length : 0;
+  return this.length;
+};
