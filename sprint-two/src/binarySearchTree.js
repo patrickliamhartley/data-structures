abf0c8b2edd9c22d5b56664f1d2ac2833f0e7608
patrickliamhartley <patrickliamhartley@gmail.com>
2016-07-29 16:16:26 -0700

    Implement insert method to binary search trees

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,74 @@
 var BinarySearchTree = function(value) {
+
+  var tree = Object.create(BinarySearchTree.methods);
+  
+  tree.value = value;
+  tree.right = null;
+  tree.left = null;
+
+  return tree;
 };
 
+BinarySearchTree.methods = {};
+
+
+
+BinarySearchTree.methods.insert = function (val) { 
+  // var check = function (node) {
+  //   if (node.value < val) {
+  //     if (node.right === null) {
+  //       node.right = BinarySearchTree(val);
+  //     } else {
+  //       check(node.right);
+  //     }
+  //   } else {
+  //     if (node.left === null) {
+  //       node.left = BinarySearchTree(val);
+  //     } else {
+  //       check(node.left);
+  //     }
+  //   }
+  // };
+  // check(this);
+  
+
+  if (this.value < val) {
+    if (this.right === null) {
+      this.right = BinarySearchTree(val);
+    } else {
+      this.right.insert(val);
+    }
+  } else {
+    if (this.left === null) {
+      this.left = BinarySearchTree(val);
+    } else {
+      this.left.insert(val);
+    }
+  }
+  
+
+  // if (val > this.value) {
+  //   if (this.right !== null) {
+  //     this.right.insert(val);
+  //   } else {
+  //     this.right = BinarySearchTree(val);
+  //   }
+  // } else {
+  //   if (this.left !== null) {
+  //     this.left.insert(val);
+  //   } else {
+  //     this.left = BinarySearchTree(val);
+  //   }
+  // }
+
+  
+};
+
+BinarySearchTree.methods.contains = function (node) { 
+};
+
+BinarySearchTree.methods.depthFirstLog = function (node) { 
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
