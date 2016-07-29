86a72eb9f37b78dcfbdb05b616033b614d726b0e
patrickliamhartley <patrickliamhartley@gmail.com>
2016-07-28 19:13:03 -0700

    Implement set and analyze complexity of methods

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,21 +1,31 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
-  set._storage = undefined;
+  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item) {
+  if (this._storage[item] && this._storage[item] === item) {
+    return true;
+  } else {
+    return false;
+  }
+
 };
 
 setPrototype.remove = function(item) {
+  delete this._storage[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
- */
+ * add= O(1)
+ * contains = O(1)
+ * remove = O(1)
+  */
